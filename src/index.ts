323561c433cadd109144b1e58e794fcfdef231b0
flaaaps <julianwening4@gmail.com>
2021-02-09 14:46:29 +0100

    :bug: Handle possible errors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import "./utils/date.js";
+import Fixture from "./models/Fixture.js";
+import { createConnection } from "./database.js";
 
 const FOOTBALL_API_KEY = process.env.FOOTBALL_API_KEY;
 
@@ -20,21 +22,33 @@ interface Fixture {
     seasons: object[];
 }
 
-const job = schedule.scheduleJob("0 0 0 * * *", function () {
+const job = schedule.scheduleJob("0 0 0 * * *", async function () {
     start();
 });
 
 if (process.env.NODE_ENV === "development") start();
 
 async function start() {
+    try {
+        await createConnection();
+    } catch (error) {
+        console.log("Error while creating connection to database:", error);
+    }
     const leagues = await getLeagues();
     const fixturePromises = leagues.map(async league => {
         return { leagueId: league.id, items: await upcomingMatches(league.id) };
     });
-
     const fixtures = await Promise.all(fixturePromises);
-
-    console.log(fixtures, "FIXTURES!");
+    fixtures.forEach(fixture => {
+        const fixtureToSave = new Fixture({
+            leagueId: fixture.leagueId,
+            items: fixture.items,
+        });
+        fixtureToSave.save(err => {
+            if (err) return console.log("Error while saving fixture!", err);
+            console.log(`Fixture with id ${fixtureToSave.leagueId} saved.`);
+        });
+    });
 }
 
 async function upcomingMatches(leagueId: number, days = 30) {
@@ -43,15 +57,18 @@ async function upcomingMatches(leagueId: number, days = 30) {
 
     const url = `https://v3.football.api-sports.io/fixtures?league=${leagueId}&season=2020&from=${dates[0]}&to=${dates[1]}`;
 
-    const {
-        data: { response },
-    } = await axios.get(url, {
-        headers: {
-            "X-RapidAPI-Key": FOOTBALL_API_KEY,
-        },
-    });
-
-    return response;
+    try {
+        const {
+            data: { response },
+        } = await axios.get(url, {
+            headers: {
+                "X-RapidAPI-Key": FOOTBALL_API_KEY,
+            },
+        });
+        return response;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
 async function getLeagues() {
@@ -59,25 +76,30 @@ async function getLeagues() {
 
     const leagues: League[] = [];
     const leaguesToSearch = [2, 78, 79, 81]; // Pull from db
-    const { data } = await axios.get(`https://v3.football.api-sports.io/leagues`, {
-        headers: {
-            "X-RapidAPI-Key": FOOTBALL_API_KEY,
-        },
-    });
 
-    const fixtures: Fixture[] = data.response;
-    const filtered = fixtures.filter((fixture: Fixture) => leaguesToSearch.includes(fixture.league.id));
+    try {
+        const { data } = await axios.get(`https://v3.football.api-sports.io/leagues`, {
+            headers: {
+                "X-RapidAPI-Key": FOOTBALL_API_KEY,
+            },
+        });
 
-    filtered.forEach(el => {
-        const league = {
-            id: el.league.id,
-            name: el.league.name,
-            type: el.league.type,
-            logo: el.league.logo,
-            country: el.country,
-            currentSeason: el.seasons[el.seasons.length - 1],
-        };
-        leagues.push(league);
-    });
-    return leagues; // store in db
+        const fixtures: Fixture[] = data.response;
+        const filtered = fixtures.filter((fixture: Fixture) => leaguesToSearch.includes(fixture.league.id));
+
+        filtered.forEach(el => {
+            const league = {
+                id: el.league.id,
+                name: el.league.name,
+                type: el.league.type,
+                logo: el.league.logo,
+                country: el.country,
+                currentSeason: el.seasons[el.seasons.length - 1],
+            };
+            leagues.push(league);
+        });
+        return leagues; // store in db
+    } catch (error) {
+        throw new Error(error);
+    }
 }
