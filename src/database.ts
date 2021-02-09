9082386538516b9711258ad87aa7f0c72248a278
flaaaps <julianwening4@gmail.com>
2021-02-09 14:47:08 +0100

    :rocket: Add database connection function

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI!, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }, err => {
-    if (err) {
-        console.log("Failed to connect to db");
-        console.log(err);
-        return;
-    }
-    console.log("Connected to db");
-});
+export function createConnection() {
+    mongoose.connect(process.env.MONGO_URI!, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }, err => {
+        if (err) {
+            console.log("Failed to connect to db");
+            console.log(err);
+            return;
+        }
+        console.log("Connected to db");
+    });
+}
