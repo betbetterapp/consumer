22e7b8e5d49661a2f8243156ef4eb2ad0dc99c23
moritz <theincxption@gmail.com>
2021-02-11 22:18:00 +0100

    :art: Improve insertion and updating of fixtures and leagues

--- src/database.ts
+++ src/database.ts
@@ -23,31 +23,9 @@ export function createConnection() {
 }
 
 export function insertFixtureCollection(data: Fixture) {
-    const fixtureToSave = new FixtureCollectionModel(data);
-    return new Promise(async (resolve, reject) => {
-        try {
-            await FixtureCollectionModel.findOneAndDelete({ leagueId: data.leagueId });
-        } catch (error) {
-            console.log("Error while deleting document:", error);
-        }
-        fixtureToSave.save(error => {
-            if (error) reject(error);
-            resolve(`Fixtures for league with id ${fixtureToSave.leagueId} saved.`);
-        });
-    });
+    return FixtureCollectionModel.updateOne({ leagueId: data.leagueId }, { $set: data }, { upsert: true, new: true });
 }
 
 export async function insertLeague(data: League) {
-    const leagueToSave = new LeagueModel(data);
-    return new Promise<string>(async (resolve, reject) => {
-        try {
-            await LeagueModel.findOneAndDelete({ id: data.id });
-        } catch (error) {
-            console.log("Error while deleting document:", error);
-        }
-        leagueToSave.save(error => {
-            if (error) reject(2); // TODO: Return string & send full error with telegram bot
-            resolve(`League with id ${leagueToSave.id} saved.`);
-        });
-    });
+    return LeagueModel.updateOne({ id: data.id }, { $set: data }, { upsert: true, new: true });
 }
