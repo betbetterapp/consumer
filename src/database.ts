2e1617c38b4d0b688d3798d1dfa1c3004a5e12bc
flaaaps <julianwening4@gmail.com>
2021-02-11 16:04:00 +0100

    :bug: Handle database connection inside promise

--- src/database.ts
+++ src/database.ts
@@ -2,13 +2,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.MONGO_URI);
+
 export function createConnection() {
-    mongoose.connect(process.env.MONGO_URI!, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }, err => {
-        if (err) {
-            console.log("Failed to connect to db");
-            console.log(err);
-            return;
-        }
-        console.log("Connected to db");
+    return new Promise((resolve, reject) => {
+        mongoose.connect(process.env.MONGO_URI!, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }, err => {
+            if (err) {
+                console.log("Error db", err);
+                reject(err);
+            } else {
+                console.log("Connected to db");
+                resolve("Connected to db");
+            }
+        });
     });
 }
