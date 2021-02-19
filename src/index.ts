2aea19003dcceae5e6358f30a2fdecb159428273
flaaaps <julianwening4@gmail.com>
2021-02-19 10:26:11 +0100

    :art: Format with updated prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,92 @@
-import schedule from "node-schedule";
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+import schedule from "node-schedule"
+import axios from "axios"
+import dotenv from "dotenv"
+dotenv.config()
 
-import "./utils/date.js";
-import * as db from "./database.js";
-import { scheduleLivePulling } from "./live-fixtures/live-fixtures.js";
+import "./utils/date.js"
+import * as db from "./database.js"
+import { scheduleLivePulling } from "./live-fixtures/live-fixtures.js"
 
-const FOOTBALL_API_KEY = process.env.FOOTBALL_API_KEY;
-const FOOTBALL_API_BASE_URL = "https://v3.football.api-sports.io";
+const FOOTBALL_API_KEY = process.env.FOOTBALL_API_KEY
+const FOOTBALL_API_BASE_URL = "https://v3.football.api-sports.io"
 const job = schedule.scheduleJob("0 0 0 * * *", async function () {
-    start();
-});
+    start()
+})
 
 export interface Fixture {
-    fixture: any,
-    league: any,
-    teams: any,
-    goals: any,
-    score: any,
+    fixture: any
+    league: any
+    teams: any
+    goals: any
+    score: any
 }
 
 export interface League {
-    id: number;
-    name: string;
-    type: string;
-    logo: string;
-    country: object;
-    currentSeason: object;
-    lastUpdated: Number;
+    id: number
+    name: string
+    type: string
+    logo: string
+    country: object
+    currentSeason: object
+    lastUpdated: Number
 }
 
 if (process.env.NODE_ENV === "development") {
-    start();
+    start()
 } else {
-    console.log(`No default execute because environment is set to ${process.env.NODE_ENV}.`);
+    console.log(`No default execute because environment is set to ${process.env.NODE_ENV}.`)
 }
 
 async function start() {
     await db.createConnection().then(async e => {
-        console.log("Database stuff happened:", e);
-        const leagues = await getLeagues();
+        console.log("Database stuff happened:", e)
+        // const leagues = await getLeagues();
 
-        for (const league of leagues) {
-            await upcomingMatches(league.id);
-        }
-    });
+        // for (const league of leagues) {
+        //     await upcomingMatches(league.id);
+        // }
+    })
     await scheduleLivePulling()
 }
 
 async function upcomingMatches(leagueId: number, days = 30) {
-    console.log("Fetching upcoming matches");
-    const dates = [new Date(), new Date().addDays(days)].map(date => date.parse());
+    console.log("Fetching upcoming matches")
+    const dates = [new Date(), new Date().addDays(days)].map(date => date.parse())
 
-    const url = `${FOOTBALL_API_BASE_URL}/fixtures?league=${leagueId}&season=2020&from=${dates[0]}&to=${dates[1]}`;
+    const url = `${FOOTBALL_API_BASE_URL}/fixtures?league=${leagueId}&season=2020&from=${dates[0]}&to=${dates[1]}`
     const {
         data: { response },
     } = await axios.get(url, {
         headers: {
             "X-RapidAPI-Key": FOOTBALL_API_KEY,
         },
-    });
+    })
 
     for (const item of response) {
-        await db.insertFixture(item)
+        await db
+            .insertFixture(item)
             .then(() => console.log(`Inserted fixture ${item.fixture.id} into database`))
-            .catch(err => console.error(err));
+            .catch(err => console.error(err))
     }
 }
 
 async function getLeagues() {
-    console.log("Fetching leagues");
+    console.log("Fetching leagues")
 
-    const leagues: League[] = [];
-    const leaguesToSearch = [2, 78, 79, 81]; // Pull from db
+    const leagues: League[] = []
+    const leaguesToSearch = [2, 78, 79, 81] // Pull from db
 
-    console.log("Going for the football api call...");
+    console.log("Going for the football api call...")
     const { data } = await axios.get(`${FOOTBALL_API_BASE_URL}/leagues`, {
         headers: {
             "X-RapidAPI-Key": FOOTBALL_API_KEY,
         },
-    });
+    })
 
-    const fixtures = data.response;
-    console.log("Got fixtures");
-    const filtered = fixtures.filter((fixture: any) => leaguesToSearch.includes(fixture.league.id));
-    console.log("Filtered fixtures");
+    const fixtures = data.response
+    console.log("Got fixtures")
+    const filtered = fixtures.filter((fixture: any) => leaguesToSearch.includes(fixture.league.id))
+    console.log("Filtered fixtures")
     filtered.forEach((el: any) => {
         const league: League = {
             id: el.league.id,
@@ -95,11 +96,11 @@ async function getLeagues() {
             country: el.country,
             currentSeason: el.seasons[el.seasons.length - 1],
             lastUpdated: Date.now(),
-        };
+        }
         db.insertLeague(league)
             .then(res => console.log(res))
-            .catch(err => console.log(err));
-        leagues.push(league);
-    });
-    return leagues;
+            .catch(err => console.log(err))
+        leagues.push(league)
+    })
+    return leagues
 }
--- src/live-fixtures/live-fixtures.ts
+++ src/live-fixtures/live-fixtures.ts
@@ -1,5 +1,5 @@
-import FixtureModel from "../models/FixtureModel.js";
-import { Fixture } from "../index.js";
+import FixtureModel from "../models/FixtureModel.js"
+import { Fixture } from "../index.js"
 
 export async function scheduleLivePulling() {
     console.log("== Scheduling live pulling ==")
@@ -7,19 +7,43 @@ export async function scheduleLivePulling() {
     const fixtures = await getAllFixtureItems()
     const fixturesToday = fixtures.filter(fixture => {
         const date = new Date(fixture.fixture.timestamp * 1000)
-        const today = new Date(1613826538000)
+        const today = new Date(1613826538000) // remove this after testing
 
         return date.isSameDay(today)
     })
 
+    const groupedByLeague = groupItemBy(fixturesToday, "league.id")
+
+    let groupedByLeagueAndDate: object = {}
+    Object.keys(groupedByLeague).forEach(fixture => {
+        console.log("One fixture:", fixture)
+        groupedByLeagueAndDate[fixture] = groupedByLeague[fixture].sort((a, b) => {
+            return a.fixture.timestamp - b.fixture.timestamp
+        })
+    })
+
+    console.log(groupedByLeagueAndDate)
+
     console.log(fixturesToday.map(fixture => `${fixture.teams.home.name} vs ${fixture.teams.away.name}`).join("\n"))
 }
 
+function groupItemBy(array: any, property: any) {
+    var hash = {},
+        props = property.split(".")
+    for (var i = 0; i < array.length; i++) {
+        var key = props.reduce(function (acc: any, prop: any) {
+            return acc && acc[prop]
+        }, array[i])
+        if (!hash[key]) hash[key] = []
+        hash[key].push(array[i])
+    }
+    return hash
+}
+
 function getAllFixtureItems(): Promise<Fixture[]> {
-    return FixtureModel.find()
-        .catch(err => {
-            console.error("An error occurred while loading all fixture items from the database")
-            console.error(err);
-            return []
-        })
+    return FixtureModel.find().catch(err => {
+        console.error("An error occurred while loading all fixture items from the database")
+        console.error(err)
+        return []
+    })
 }
