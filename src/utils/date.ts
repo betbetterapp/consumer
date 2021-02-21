c45037ccf2e0bbb6c1fd88ae7615c25fb89134cc
flaaaps <julianwening4@gmail.com>
2021-02-21 19:00:50 +0100

    :rocket: Create prototype function to format a date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,20 +4,21 @@ export {}
 
 declare global {
     interface Date {
-        addDays(days: number): Date;
-        parse(): String;
-        isSameDay(other: Date): boolean;
+        addDays(days: number): Date
+        parse(): String
+        isSameDay(other: Date): boolean
         formatClock(withSeconds?: boolean): string
+        formatDate(reverse: boolean)
     }
 }
 
-Date.prototype.addDays = function(days: number): Date {
+Date.prototype.addDays = function (days: number): Date {
     const date = new Date(this.valueOf())
     date.setDate(date.getDate() + days)
     return date
 }
 
-Date.prototype.parse = function(): String {
+Date.prototype.parse = function (): String {
     const date = new Date(this)
     const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date)
     const mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date)
@@ -25,11 +26,11 @@ Date.prototype.parse = function(): String {
     return `${ye}-${mo}-${da}`
 }
 
-Date.prototype.isSameDay = function(b: Date): boolean {
+Date.prototype.isSameDay = function (b: Date): boolean {
     return this.getDate() === b.getDate() && this.getMonth() == b.getMonth() && this.getFullYear() == b.getFullYear()
 }
 
-Date.prototype.formatClock = function(withSeconds: boolean = false): string {
+Date.prototype.formatClock = function (withSeconds: boolean = false): string {
     const hh = this.getHours().prependZeroes(2)
     const mm = this.getMinutes().prependZeroes(2)
 
@@ -39,4 +40,19 @@ Date.prototype.formatClock = function(withSeconds: boolean = false): string {
     } else {
         return `${hh}:${mm}`
     }
+}
+
+Date.prototype.formatDate = function (reverse: boolean) {
+    const date = new Date(this)
+    let dateString = `${date.getDate()}-${formatMonth(date.getMonth() + 1)}-${date.getFullYear()}`
+    if (reverse) dateString = dateString.split("-").reverse().join("-")
+    return dateString
+}
+
+function formatMonth(month) {
+    month = month.toString()
+    if (month.length < 2) {
+        return (month = "0" + month)
+    }
+    return month
 }
